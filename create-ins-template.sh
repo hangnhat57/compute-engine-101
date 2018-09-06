426f01d24f1e11d219b41aa056fd04ce9e470703
Nhat Nguyen <nhat@Akita-3.local>
2018-09-06 09:53:26 +0700

    Update for self deploy

--- create-ins-template.sh
+++ create-ins-template.sh
@@ -1,9 +1,9 @@
 gcloud compute instance-templates create google-compute-101 \
 --machine-type=n1-standard-1 \
---network=projects/<project-id>/global/networks/default \
+--network=projects/corded-protocol-215406/global/networks/default \
 --network-tier=PREMIUM \
 --maintenance-policy=MIGRATE \
---service-account=<service-account-name> \
+--service-account=compute-engine-101@corded-protocol-215406.iam.gserviceaccount.com \
 --scopes=https://www.googleapis.com/auth/cloud-platform \
 --tags=http-server \
 --image=debian-9-stretch-v20180820 \
